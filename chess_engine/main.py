16c235ccbd5ac3ee5dafed4b814bdb8bc31534ab
andrussults <andrussults@gmail.com>
2024-02-07 14:29:59 +0200

    test

--- chess_engine/main.py
+++ chess_engine/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import chess
 
 app = Flask(__name__)
 
@@ -6,8 +7,21 @@ app = Flask(__name__)
 books = [
     {"id": 1, "title": "Python Programming", "author": "Guido van Rossum"},
     {"id": 2, "title": "Flask Web Development", "author": "Miguel Grinberg"},
-    {"id": 3, "title": "Django for Beginners", "author": "William S. Vincent"}
+    {"id": 3, "title": "Django for Beginners", "author": "William S. Vincent"},
+    {"id": 4, "title": "Python Programming", "author": "Guido van Rossum"},
+    {"id": 5, "title": "Flask Web Development", "author": "Miguel Grinberg"},
+    {"id": 6, "title": "Django for Beginners", "author": "William S. Vincent"}
 ]
+'''
+current_board = [
+    print(chess.BaseBoard())
+
+]
+
+@app.route('/currentboard', methods=['GET'])
+def get_books():
+    return jsonify(current_board)
+'''
 
 @app.route('/books', methods=['GET'])
 def get_books():
