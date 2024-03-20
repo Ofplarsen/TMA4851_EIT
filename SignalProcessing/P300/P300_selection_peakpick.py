b789a054002ef1fad4037285d8c40f0876d237a4
wanjing321 <128313618+wanjing321@users.noreply.github.com>
2024-03-20 14:17:38 +0100

    P300 code

---
+++ SignalProcessing/P300/P300_selection_peakpick.py
@@ -0,0 +1,205 @@
+import mat73
+import scipy
+import scipy.io as sio # cannot use for v7.3 mat file
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import signal_functions as sf
+
+def MakePeak(check):
+   '''
+   Takes in electrode to analyze, return (max. amplitude - min. amplitude) after averaging over all events for one electrode
+   '''
+   max_check = np.max(check, axis=2)  #Average over all non-target events; TODO: check if possible to do max over all events then pick out avg
+   peak = np.max(max_check[:, 200:], axis=1) - np.min(max_check[:, 0:200], axis=1)
+   '''
+   #If a negative answer, the find the peak using the event periode
+   if np.min(peak) <= 0:
+      peak = np.max(max_check[:, 220:500], axis=1) - np.min(max_check[:, 220:500], axis=1)
+   else:
+      pass
+   '''
+   #print("non-target peak list:", peak)
+   return peak
+
+def ClassificationWithPeak(peak_nontarget, check, threshold, n, j):
+   '''
+   Peak_nontarget: must be obtained with non-target training set, a list, one peak for each electrode
+   check: data array not sorted after label
+   threshold: the percentage of electrodes that needs to be positive to conclude a target event
+   j: specify the position of event in dataset
+   '''
+   TargetElectrode = 0
+   for electrode in range(n):
+      event_to_test = check[electrode,:,j]
+      amp = np.max(event_to_test) - np.min(event_to_test)
+      if peak_nontarget[electrode] < amp:
+         TargetElectrode += 1
+      else:
+         continue
+   b = TargetElectrode/n
+   #print("percentage positive electrodes", b, "Event number", j)
+   if b > threshold:
+      return True
+   else:
+      return False
+   
+#print("target percentage", tgtpercentage)
+#print("ntp:", ntp)
+#print(ClassificationWithPeak(ntp, nosortuse, 0.6, 32, 1))   #This is only one classification
+
+#Now check percentage correct classification
+def CheckClassPercentage(ntp, nosort, threshold, n, marker):
+   '''
+   nontarget: data array for non-target events
+   nosort: data array not sorted after label
+   threshold: the percentage of electrodes that needs to be positive to conclude a target event
+   n: number of electrodes
+   '''
+   correct = 0
+   true = 0
+   false = 0
+   for i in range(len(nosort[0,0,:])):
+      truetrue = len(np.where(marker==1)[0])*2
+      truefalse = len(np.where(marker==2)[0])*2
+      if ClassificationWithPeak(ntp, nosort, threshold, n, i):
+         true += 1
+         if i < 900:
+            if marker[i] == 1:
+               #print("Truetrue index found", i)
+               correct += 1
+         else:
+            index = i - 900
+            if marker[index] == 1:
+               correct += 1
+      else:
+         false += 1
+   #print('Known True false', truefalse)
+   #print('Classified false', false)
+   #print("Known True targets", truetrue)
+   #print("Classified True targets", true)
+   return correct/true
+
+def OptimizeThresholdSimpel(nontarget, nosort, n):
+   '''
+   nontarget: data array for non-target events
+   nosort: data array not sorted after label
+   n: number of electrodes
+   '''
+   threshold = np.linspace(0.6, 0.99, 60)
+   percentage = []
+   print("Running optimization")
+   for t in threshold:
+      perc = CheckClassPercentage(nontarget, nosort, t, n, marker)
+      if perc < 1:
+         percentage.append(perc)
+      else:
+         percentage.append(0)
+
+   for p in percentage:
+      if p == max(percentage):
+         index = percentage.index(p)
+   return threshold[index]
+
+
+def PerformaceElectrode(ntg, nosort, marker, e):
+   '''
+   return a list of best performing electrodes
+   e: number of electrodes to select
+   '''
+   true = len(np.where(marker==1)[0])*2
+   scorelist = []
+   for electrode in range(n):
+      correct = 0
+      for j in range(1800):
+         event_to_test = nosort[electrode,:,j]
+         amp = np.max(event_to_test) - np.min(event_to_test)
+         if ntg[electrode] < amp:
+            #then classify as true, check with marker
+            if j < 900:
+               if marker[j] == 1:
+                  correct += 1
+            else:
+               index = j - 900
+               if marker[index] == 1:
+                  correct += 1
+      score = correct/true
+      if score < 1:
+         scorelist.append(score)
+      else:
+         scorelist.append(0)
+
+   score_array = np.array(scorelist)
+   sorted_indices = np.argsort(score_array)[::-1]
+   top_n_indices = sorted_indices[:e]
+   return top_n_indices
+
+   
+
+#[31, 32, 12, 13, 19, 16, 11, 14, 18, 20]
+#selection = [30, 31, 11, 12, 18, 15, 10, 13, 17, 19, 0, 29, 1, 28, 14, 16]
+selection = np.linspace(0, 31, 32, dtype=int)
+ImprovedSelection = [10, 2, 17, 21, 22, 25, 23, 24, 30, 6, 9, 4, 19, 20, 27]
+n = len(selection)
+
+
+nontargetuse, targetuse, nosortuse, marker = sf.ReadFile("TMA4851_EIT\SignalProcessing\P300\data\s53.mat", 0, 600, selection)
+ntp = MakePeak(nontargetuse)
+
+def SelectSignal(nontarget, target, n, permute):
+   '''
+   n signals in total, where only one is target signal
+   Turn on (1) and off (0) permute to shuffle the signals
+   '''
+
+   targetSignal = target[:,:,299]
+   nontargetSignal = nontarget[:,102:,1:n]
+   # Stack the signals along the third dimension
+   stackedSignal = np.dstack((targetSignal, nontargetSignal))
+
+   # Generate a random permutation of the third axis
+   if permute == 1:
+      perm = np.random.permutation(stackedSignal.shape[2])
+   # Apply the permutation to the third axis
+      stackedSignal = stackedSignal[:,:,perm]
+
+   return stackedSignal
+
+def evaluate_Y(n):
+   '''
+   n signals in total, where only one is target signal
+   '''
+   selection = np.linspace(0, 31, 32, dtype=int)
+   nontargetuse, targetuse, nosortuse, marker = sf.ReadFile("TMA4851_EIT\SignalProcessing\P300\data\s53.mat", 0, 600, selection)
+   SS = SelectSignal(nontargetuse, targetuse, n, 1)
+   
+   #One run for Col index
+   for j in range(n):
+      if ClassificationWithPeak(ntp, SS, 0.7446, 32, j):
+         indexCol = j
+
+   #Regenerate mock signal for row index
+   SS = SelectSignal(nontargetuse, targetuse, n, 1)
+   for j in range(n):
+      if ClassificationWithPeak(ntp, SS, 0.7446, 32, j):
+         indexRow = j
+
+   return [indexRow, indexCol]
+#print("Performance checked list", PerformaceElectrode(ntp, nosortuse, marker, 15))
+
+#print(get_Y(6))
+
+#print(CheckClassPercentage(ntp, nosortuse, 0.8446, n, marker))
+#print("Optimal threshold", OptimizeThresholdSimpel(ntp, targetuse, n))
+# % = 0.8776 for s53, 0.8446 for s01
+#Note: when increase range to 0,600, true = 0, not possible to optimize, bad success percentage
+
+
+def Test():
+   nontargetuse, _, nosortuse, tgtpercentage = sf.ReadFileTest("data/s01.mat", 32, 220, 500)
+   ntp = MakePeak(nontargetuse)
+   print("target percentage", tgtpercentage)
+   print(ClassificationWithPeak(ntp, nosortuse, 0.877, 32, 1))   #This is only one classification
+   print("percentage correct classification", CheckClassPercentage(ntp, nosortuse, 0.752, 32))
+#Test()
---
+++ SignalProcessing/P300/signal_functions.py
@@ -0,0 +1,239 @@
+# filtering
+from scipy.signal import butter, filtfilt
+import numpy as np
+import matplotlib.pyplot as plt
+import mat73
+
+
+
+def butter_lowpass_filter(data, lowcut, fs, order):
+  nyq = fs/2
+  low = lowcut/nyq
+  b, a = butter(order, low, btype='low')
+  y = filtfilt(b, a, data) # zero-phase filter # data: [ch x time]
+  return y
+
+def butter_highpass_filter(data, highcut, fs, order):
+  nyq = fs/2
+  high = highcut/nyq
+  b, a = butter(order, high, btype='high')
+  y = filtfilt(b, a, data) # zero-phase filter
+  return y
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order):
+  nyq = fs/2
+  low = lowcut/nyq
+  high = highcut/nyq
+  b, a = butter(order, [low, high], btype='band')
+  # demean before filtering
+  meandat = np.mean(data, axis=1)
+  data = data - meandat[:, np.newaxis]
+  y = filtfilt(b, a, data)
+  return y
+
+def extractEpoch3D(data, event, srate, baseline, frame, opt_keep_baseline):
+  # extract epoch from 2D data into 3D [ch x time x trial]
+  # input: event, baseline, frame
+  # extract epoch = baseline[0] to frame[2]
+
+  # for memory pre-allocation
+  if opt_keep_baseline == True:
+    begin_tmp = int(np.floor(baseline[0]/1000*srate))
+    end_tmp = int(begin_tmp+np.floor(frame[1]-baseline[0])/1000*srate)
+  else:
+    begin_tmp = int(np.floor(frame[0]/1000*srate))
+    end_tmp = int(begin_tmp+np.floor(frame[1]-frame[0])/1000*srate)
+  
+  epoch3D = np.zeros((data.shape[0], end_tmp-begin_tmp, len(event)))
+  nth_event = 0
+
+  for i in event:
+    if opt_keep_baseline == True:
+      begin_id = int(i + np.floor(baseline[0]/1000 * srate))
+      end_id = int(begin_id + np.floor((frame[1]-baseline[0])/1000*srate))
+    else:
+      begin_id = int(i + np.floor(frame[0]/1000 * srate))
+      end_id = int(begin_id + np.floor((frame[1]-frame[0])/1000*srate))
+    
+    tmp_data = data[:, begin_id:end_id]
+
+    begin_base = int(np.floor(baseline[0]/1000 * srate))
+    end_base = int(begin_base + np.floor(np.diff(baseline)/1000 * srate)-1)
+    base = np.mean(tmp_data[:, begin_base:end_base], axis=1)
+
+    rmbase_data = tmp_data - base[:, np.newaxis]
+    epoch3D[:, :, nth_event] = rmbase_data
+    nth_event = nth_event + 1
+
+  return epoch3D
+
+#Find average and standard deviation of nontarget amplitudes
+
+def PlotDiff():
+   #Differanse mellom avg nontarget and avg target
+   fig, ax = plt.subplots(8,1)
+   #difference = targetEEGuse - nontargetEEGuse
+   avg_nontarget = np.mean(nontargetEEGuse, axis=2) 
+   avg_target = np.mean(targetEEGuse, axis = 2)
+   difference = avg_target - avg_nontarget
+   #max_nontarget = np.max(nontargetEEGuse, axis = 2)
+   #min_target = np.min(targetEEGuse, axis = 2)
+   #difference = min_target - max_nontarget
+   t = np.linspace(-200, 1000, min_target.shape[1])
+   df_avg = pd.DataFrame(index=t, data=difference.T, columns=np.arange(0, 8))
+   df_avg.plot()
+   plt.grid()
+   plt.show()
+   #df_mindiff = pd.DataFrame(index=t, data=mindiff.T, columns=np.arange(0, 8))
+   #df_mindiff.plot()
+   #plt.legend()
+   #plt.grid()
+   #plt.show()
+
+#PlotDiff()
+
+def plotdata(start_time, end_time, test, nontarget, n):
+   fig, ax = plt.subplots(8,1)
+   for i in range(n):
+      #nontarget_std = np.std(nontarget[i, :, :], axis=1)
+      avg_nontarget = np.mean(nontarget[i, :, :], axis=1) 
+      avg_test = np.mean(test[i, :, :], axis = 1)
+      t = np.linspace(start_time, end_time, avg_test.shape[0])
+      ax[i].plot(t, avg_nontarget, color="red", label="Non-target")
+      ax[i].plot(t, avg_test, color="blue", label="Target")
+      ax[i].legend()
+   plt.show()
+
+def ReadFile(filename, read_start, read_end, selection):
+   '''
+   filename as string, n is number of electrodes
+   read_start: start time (ms) where readings is interesting
+   read_end: end time (ms)
+   selection: list of electrodes selected
+   '''
+   #Read .mat file
+   EEG = mat73.loadmat(filename)
+   EEG.keys()
+   EEG['RSVP'].keys()
+   EEG['test'][0].keys()
+   EEG['train'][0].keys()
+
+   #Save data as lists
+   baseline = [-200, 0] # in ms
+   frame = [read_start, read_end] # in ms
+   for n_calib in range(len(EEG['train'])):
+      data = np.asarray(EEG['train'][n_calib]['data'])
+      srate = EEG['train'][n_calib]['srate']
+      data = butter_bandpass_filter(data, 0.5, 10, srate, 4)
+      markers = EEG['train'][n_calib]['markers_target']
+      print(data.shape)
+      print(markers.shape)
+
+      targetID = np.where(markers==1)[0]
+      nontargetID = np.where(markers==2)[0]
+      #extraID = np.where(markers==0)[0]
+      allID = np.where(markers > 0)[0]
+      tmp_nosortEEG = extractEpoch3D(data, allID, srate, baseline, frame, False)
+      tmp_targetEEG = extractEpoch3D(data, targetID, srate, baseline, frame, False)
+      tmp_nontargetEEG = extractEpoch3D(data, nontargetID, srate, baseline, frame, True)#Keep baseline data to extract minimun before event
+      if n_calib == 0:
+         targetEEG = tmp_targetEEG
+         nontargetEEG = tmp_nontargetEEG
+         nosortEEG = tmp_nosortEEG
+      else:
+         targetEEG = np.dstack((targetEEG, tmp_targetEEG))
+         nontargetEEG = np.dstack((nontargetEEG, tmp_nontargetEEG))   
+         nosortEEG = np.dstack((nosortEEG, tmp_nosortEEG))
+   #Extract only data for the first n electrodes
+   print('total shape target', targetEEG.shape)
+   print('total shape nontarget', nontargetEEG.shape)
+   print('shape nosort', nosortEEG.shape)
+   nontargetEEGuse = nontargetEEG[selection] 
+   targetEEGuse = targetEEG[selection]
+   nosortEEGuse = nosortEEG[selection]
+   # Get the markers for nosortEEG
+   nosortMarkers = markers[allID]
+   percentageTarget = len(targetID)/len(allID)
+   return nontargetEEGuse, targetEEGuse, nosortEEGuse, nosortMarkers
+
+def ReadFileTest(filename,n, read_start, read_end):
+   '''
+   filename as string, n is number of electrodes
+   read_start: start time (ms) where readings is interesting
+   read_end: end time (ms)
+   '''
+   #Read .mat file
+   EEG = mat73.loadmat(filename)
+
+   #Save data as lists
+   baseline = [-200, 0] # in ms
+   frame = [read_start, read_end] # in ms
+   for n_test in range(len(EEG['test'])):
+      data = np.asarray(EEG['test'][n_test]['data'])
+      srate = EEG['test'][n_test]['srate']
+      data = butter_bandpass_filter(data, 0.5, 10, srate, 4)
+      markers = EEG['test'][n_test]['markers_target']
+
+      targetID = np.where(markers==1)[0]
+      nontargetID = np.where(markers==2)[0]
+      allID = np.append(targetID, nontargetID)
+      tmp_nosortEEG = extractEpoch3D(data, allID, srate, baseline, frame, False)
+      tmp_targetEEG = extractEpoch3D(data, targetID, srate, baseline, frame, False)
+      tmp_nontargetEEG = extractEpoch3D(data, nontargetID, srate, baseline, frame, True)#Keep baseline data to extract minimun before event
+      if n_test == 0:
+         targetEEG = tmp_targetEEG
+         nontargetEEG = tmp_nontargetEEG
+         nosortEEG = tmp_nosortEEG
+      else:
+         targetEEG = np.dstack((targetEEG, tmp_targetEEG))
+         nontargetEEG = np.dstack((nontargetEEG, tmp_nontargetEEG))   
+         nosortEEG = np.dstack((nosortEEG, tmp_nosortEEG))
+   #Extract only data for the first n electrodes
+   nontargetEEGuse = nontargetEEG[:n] 
+   targetEEGuse = targetEEG[:n]
+   nosortEEGuse = nosortEEG[:n]
+   percentageTarget = len(targetID)/len(allID)
+   # Convert markers to a 2D array where each row corresponds to an electrode
+   markers_2D = np.tile(markers, (n, 1))
+   # Save marker data for each event for every electrode, first 20 events are 0, not marked
+   markerUse = [markers_2D[i, 20:1820] for i in range(n)]
+   #Pakking av markers: stacke events på electrode
+   return nontargetEEGuse, targetEEGuse, nosortEEGuse, markerUse
+
+def OrigReadFile(filename, read_start, read_end):
+   '''
+   filename as string, n is number of electrodes
+   read_start: start time (ms) where readings is interesting
+   read_end: end time (ms)
+   Original function from open source code
+   '''
+   #Read .mat file
+   EEG = mat73.loadmat(filename)
+   EEG.keys()
+   EEG['RSVP'].keys()
+   EEG['test'][0].keys()
+   EEG['train'][0].keys()
+
+   #Save data as lists
+   baseline = [-200, 0] # in ms
+   frame = [read_start, read_end] # in ms
+# pre-processing for test data
+   for n_test in range(len(EEG['test'])):
+    data = np.asarray(EEG['test'][n_test]['data'])
+    srate = EEG['test'][n_test]['srate']
+    data = butter_bandpass_filter(data, 0.5, 10, srate, 4)
+    markers = EEG['test'][n_test]['markers_target']
+   
+    targetID = np.where(markers==1)[0]
+    nontargetID = np.where(markers==2)[0]
+   
+    tmp_targetEEG = extractEpoch3D(data, targetID, srate, baseline,  frame, False)
+    tmp_nontargetEEG = extractEpoch3D(data, nontargetID, srate,  baseline, frame, False)
+    if n_test == 0:
+     targetEEG = tmp_targetEEG
+     nontargetEEG = tmp_nontargetEEG
+    else:
+      targetEEG = np.dstack((targetEEG, tmp_targetEEG))
+      nontargetEEG = np.dstack((nontargetEEG, tmp_nontargetEEG))   
+   # Get the markers for nosortEEG
+   return targetEEG, nontargetEEG
