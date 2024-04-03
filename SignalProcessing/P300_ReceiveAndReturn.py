2529802680d399d6356d9d86ab5222f9610cd280
wanjing321 <128313618+wanjing321@users.noreply.github.com>
2024-04-03 15:03:09 +0200

    Working code, and cleaned

--- SignalProcessing/P300_ReceiveAndReturn.py
+++ SignalProcessing/P300_ReceiveAndReturn.py
@@ -1,30 +1,18 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pyxdf
+
 import time
 from pylsl import StreamInlet, resolve_stream, resolve_byprop
 from io_utils import read_xdf
 #import filter
 from lsl_interactions import listen_for_amp, listen_for_start, send_index_data
 from pylsl import StreamInfo, StreamOutlet
+import P300.signal_functions as sf
 import P300.P300_selection_peakpick
 
 
 if __name__ == "__main__":
-    # info = StreamInfo('SingleIntSignal', 'Int', 1, 100, 'int32', 'myuid34235')
-    # Both output and input from lsl needs a info object
-    # type(setup_params) = Flicker_info
-    # listen_for_start might return a timestamp
-    # listen_for_amp is a stream that returns [timestamp, amp(timestamp)]
-    # amp_data = [[t, amp(t)]]
-    # each info-object needs an inlet or outlet object
-
-    # seed parameters:
-    #f_k_arr = np.array([4, 5, 6, 7])
-    sample_rate = 500
-    #t = np.arange(0, 2, 1/sample_rate)
-    #Y = get_Y(f_k_arr, t)
 
     backend_info = StreamInfo('IndexData', 'Marker', 2, 100, 'int32', 'IndexData')
     backend_outlet = StreamOutlet(backend_info)
@@ -32,24 +20,26 @@ if __name__ == "__main__":
     flicker_info = resolve_stream('source_id', 'MentalChess')
     flicker_inlet = StreamInlet(flicker_info[0])
 
-    amp_info = resolve_stream('source_id', 'SignalProcessing')
-    amp_inlet = StreamInlet(amp_info[0])
-
-
-
     while True:
         tid = listen_for_start(flicker_inlet)  # method defined by Hans
+        # Listen for rows
         start = time.time()
-        X_row = listen_for_amp(flicker_inlet, amp_inlet, indicator=[1])[-3000:]  # to be defined by Hans
+        #X_row = listen_for_amp(flicker_inlet, amp_inlet, indicator=[1])[-3000:]  # Listen to signal from LSL, replaced with sleep here
+        time.sleep(4.8)
         end = time.time()
+        m = round((end - start)/0.8) # Determines how many cycles to listen for, 800ms per cycle
 
-        n = round((end - start)/0.8) #800ms per cycle
-
-        X_col = listen_for_amp(flicker_inlet, amp_inlet, indicator=[2])[-3000:]
+        # Listen for columns
+        print("Between sleeps")
+        start = end
+        #X_col = listen_for_amp(flicker_inlet, amp_inlet, indicator=[2])[-3000:]  # Listen to signal from LSL, replaced with sleep here
+        time.sleep(4.8)
+        end = time.time()  
+        n =  round((end - start)/0.8)
 
         # X_row and X_col are generated from available data files in evaluate_Y instead of being analyzed directly
-        idxs = P300.P300_selection_peakpick.evaluate_Y(n)
-        print('estimated indices: ',  idxs)
+        idxs = P300.P300_selection_peakpick.evaluate_Y(m, n)
+        print('Estimated indices: ',  idxs)
         send_index_data(backend_outlet, idxs)
 
 
